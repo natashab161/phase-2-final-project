9b6bff1afe4415aeba03eacfd90eaa5c12c3f84d
Natasha Bartley <+>
2023-04-22 03:05:00 -0400

    double commit for events comps outlined

---
+++ src/EventsPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function EventsPage() {
+    return (
+        <div>
+            {/* container for <EventCards
+                I named this EventsPage bc EventsContainer was too long */}
+            {/* search will also get rendered here */}
+        </div>
+    )
+}
+
+export default EventsPage;
