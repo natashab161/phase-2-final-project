a5e4faf5c6570e03cc2540da824d3f2f364f0266
Natasha Bartley <+>
2023-04-22 01:54:14 -0400

    info.text file created

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
 import './App.css';
 import React from 'react';
+import NavBar from './NavBar';
 import Home from './Home';
 import About from './About';
-import { Link, Route, Routes } from 'react-router-dom';
+
 
 
 function App() {
   return (
     <div className="App">
-      <nav className='navBar'>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
-
-      </nav>
-        <Routes>
-          <Route path='/' element={<Home />}/>
-
-
-        </Routes>
-          
+        <NavBar />
     </div>
   );
 }
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { Link, Route, Routes, Switch } from 'react-router-dom';
+import Home from "./Home";
+import About from "./About";
 
 function NavBar () {
     return (
         <div>
-
+          <nav className="navBar">
+            <ul>
+                <li> <Link to='/'>home</Link> </li>
+                <li> <Link to='/About'>about</Link> </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/About" element={<About />}/>
+            </Routes>
+          </Switch>
         </div>
     )
 }
 
-export default NavBar
+export default NavBar;
