18dbe6a6c619ba6e2d514400f3bf9c2d52dba594
Natasha Bartley <+>
2023-04-22 03:05:00 -0400

    double commit for events comps outlined

--- src/EventsCards.jsx
+++ src/EventsCards.jsx
@@ -3,7 +3,10 @@ import React from "react";
 function EventsCard() {
     return (
         <div>
-
+            {/* i think we will fetch  the json data here */}
+            {/* photo + displayed information about each card/event */}
+            {/* useState and onClick to use the save button */}
+            {/* route to 'links' for tickets, for the MVP this route can exsist and go to  a styled  404 */}
         </div>
     )
 };
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 function Home(){
     return(
         <div>
@@ -8,7 +7,6 @@ function Home(){
             {/* Featured Events */}
             {/* Search */}
             {/* Map */}
-
         </div>
     )
 }
