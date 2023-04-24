c537f0e393419f8e9205096e99b034da384b948e
Natasha Bartley <+>
2023-04-24 13:24:03 -0400

    starter css for an event card with image and text

--- src/EventCard.jsx
+++ src/EventCard.jsx
@@ -3,7 +3,14 @@ import React from "react";
 function EventCard() {
     return (
         <div className="eventCard">
+            <img 
+                src="https://nypost.com/wp-content/uploads/sites/2/2021/08/NYC1.jpg?quality=90&strip=all&w=1024" 
+                alt="I heart nyc image" 
+                style={{ width: '100%' }} />
 
+            <div className=".eventTextContainter">
+                <p>this is an example of an event image</p>
+            </div>
             {/* i think we will fetch  the json data here */}
             {/* photo + displayed information about each card/event */}
             {/* useState and onClick to use the save button */}
