26fba35ec54a5b0f7059864d546c62e104282e77
Natasha Bartley <+>
2023-04-24 17:56:39 -0400

    newEvent data

--- src/EventForm.jsx
+++ src/EventForm.jsx
@@ -1,6 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-function EventForm(){
+function EventForm({ eventSubmit }){
+
+    function handleEventSubmit(e) {
+        e.preventDefault();
+
+        const newEvent = {
+            title: title,
+            street: street,
+            city: city,
+            state: state,
+            zipCode: zipCode,
+            neighborhood:  neighborhood,
+            venue: venue, 
+            date: calander,
+            time: time,
+            tickets: tickets,
+            price: price,
+            thumbnail: thumbnail,
+            artFrom: artFrom,
+            age: age,
+            category: category,
+            subcategory: subcategory,
+            images: images,
+            description: description,
+            collaborators:collaborators,
+            tags: tags,
+            
+ /* remeber to ask micheal if this is the correct formatting */
+        }
+    }
+
     return(
         <div>
             {/* user will submit a form that will create an EventCard and post it to EventsPage */}
