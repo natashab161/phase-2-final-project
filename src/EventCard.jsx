705deb98d210462c54d0bf4d23fc33588cf5b2ce
Natasha Bartley <+>
2023-04-24 10:35:23 -0400

    started css for the  cards

--- src/EventCard.jsx
+++ src/EventCard.jsx
@@ -2,13 +2,16 @@ import React from "react";
 
 function EventCard() {
     return (
-        <div>
+        <div className="eventCard">
+
             {/* i think we will fetch  the json data here */}
             {/* photo + displayed information about each card/event */}
             {/* useState and onClick to use the save button */}
             {/* route to 'links' for tickets, for the MVP this route can exsist and go to  a styled  404 */}
             {/* */}
+            
         </div>
+         
     )
 };
 
