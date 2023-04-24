17dd49a45d6528384022d5e445e51850a213ff9f
Natasha Bartley <+>
2023-04-24 10:41:00 -0400

    EventsPage imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
 import React from 'react';
 import NavBar from './NavBar';
+import EventsPage from './EventsPage';
 
 function App() {
   return (
     <div className="App">
         <NavBar />
-        
+        <EventsPage />
     </div>
   );
 }
