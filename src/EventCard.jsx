6ce5bf857de434b5f1e2eb37f9b4f9019bd6ae36
Natasha Bartley <+>
2023-04-24 15:01:59 -0400

    key value pairs for the event details

--- src/EventCard.jsx
+++ src/EventCard.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 
-function EventCard() {
+function EventCard({ event }) {
     return (
         <div className="eventCard">
-            <img 
-                src="https://nypost.com/wp-content/uploads/sites/2/2021/08/NYC1.jpg?quality=90&strip=all&w=1024" 
-                alt="I heart nyc image" 
-                style={{ width: '100%' }} />
+            <img className="eventThumbnail" 
+                src="https://nypost.com/wp-content/uploads/sites/2/2021/08/NYC1.jpg?quality=90&strip=all&w=1024"
+               /* src={artist.thumbnail} alt={artist.title} */
+            />
 
-            <div className=".eventTextContainter">
-                <p>this is an example of an event image</p>
+            <div className="eventTextContainter">
+              <h3 className="eventTitle">{event.title}</h3>  
+                    <div className="eventSubHeader"> 
+                        <p>this is an example of an event image, delete once json data is in</p>
+                        {/* these wont all have be h4, play around with the visual hierarchy later */}
+                    {/* <h4>{event.location.neighborhood}</h4>
+                        <h4>{event.venue}</h4>
+                        <h4>{event.calender.date}</h4>
+                        <h4>{event.age}</h4>
+                        <h4>{event.artform}</h4>  
+                     */}
+                    </div>
             </div>
-            {/* i think we will fetch  the json data here */}
-            {/* photo + displayed information about each card/event */}
+
             {/* useState and onClick to use the save button */}
             {/* route to 'links' for tickets, for the MVP this route can exsist and go to  a styled  404 */}
             {/* */}
