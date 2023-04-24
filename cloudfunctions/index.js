6531cc5ce763dace205537e5fdbb8f89fa950ed9
+ <tidydata@outlook.com>
2023-04-24 15:01:18 -0400

    Firebase function still doesn't work...

--- cloudfunctions/index.js
+++ cloudfunctions/index.js
@@ -1,9 +1,26 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin")
+const sgMail = require("@sendgrid/mail");
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+sgMail.setApiKey(functions.config().sendgrid.key);
+
+const welcomeEmail = functions.auth.user().onCreate((user) => {
+    const msg = {
+      to: user.email,
+      from: "matthewrobertmacfarlane@gmail.com", // Replace with your own email address or a verified sender
+      subject: "Welcome to pullUp NYC!",
+      text: `Welcome, ${user.displayName || user.email}! Thanks for signing up for our app. We hope you enjoy using it.`,
+      html: `<p>Welcome, <strong>${user.displayName || user.email}</strong>! Thanks for signing up for our app. We hope you enjoy using it.</p>`,
+    };
+  
+    return sgMail.send(msg).then(() => {
+      console.log("Welcome email sent to:", user.email);
+    }).catch((error) => {
+      console.error("Error sending welcome email:", error);
+    });
+  });
+  
+  exports.welcomeEmail = welcomeEmail;
+  
