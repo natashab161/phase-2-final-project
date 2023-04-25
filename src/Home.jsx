8bd953c4954b3b69ee01b663874e55b6622f30e8
Natasha Bartley <+>
2023-04-25 11:02:39 -0400

    testing out the carousel

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import EventsPage from "./EventsPage";
+import CarouselHeader from "./CarouselHeader";
 
 function Home(){
     return(
         <div>
-            {/* <h1> */}
+            <img src="./media/tester-logo.png">
+            </img>
+            <h1>
+                the place to be for local artists and art-lovers to connect and grow
+            </h1>
             {/* <FeaturedEvents /> */}
             {/* Search & Map can be nested here or within the EventsPage*/}
-            {/* Footer */}
             <EventsPage />
+            {/* Footer */}
         </div>
     )
 }
