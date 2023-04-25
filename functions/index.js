93b7c338c3fb7467e3939fd57c851359f678d71d
+ <tidydata@outlook.com>
2023-04-25 12:33:50 -0400

    detectFaces cloud function test!

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,55 @@
-const functions = require("firebase-functions");
+// Import the required libraries and initialize the Firebase app
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const { Storage } = require('@google-cloud/storage');
+const { ImageAnnotatorClient } = require('@google-cloud/vision');
+const os = require('os');
+const path = require('path');
+const fs = require('fs');
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+// Create a new storage bucket
+const storage = new Storage();
+const srcBucketName = 'pullupnyc.appspot.com';
+const dstBucketName = 'cloud-vision-faces-bucket';
+const srcBucket = storage.bucket(srcBucketName);
+const dstBucket = storage.bucket(dstBucketName);
+
+// Create a function that listens to changes in the 'images' directory
+exports.detectFaces = functions.storage.object().onFinalize(async (object) => {
+  const filePath = object.name;
+  const bucketDir = path.dirname(filePath);
+  const fileName = path.basename(filePath);
+  const tempFilePath = path.join(os.tmpdir(), fileName);
+
+  // Download the image to a temporary directory
+  await srcBucket.file(filePath).download({
+    destination: tempFilePath,
+  });
+
+  // Analyze the image using Cloud Vision API to detect faces
+  const client = new ImageAnnotatorClient();
+  const [result] = await client.faceDetection(tempFilePath);
+  const faces = result.faceAnnotations;
+
+  // Check if the image has any faces and upload to the new bucket if it does
+  if (faces.length > 0) {
+    const newFilePath = path.join(bucketDir, 'faces', fileName);
+    await dstBucket.upload(tempFilePath, {
+      destination: newFilePath,
+      metadata: {
+        contentType: 'image/jpeg',
+      },
+    });
+    console.log(`Image ${fileName} uploaded to ${dstBucketName}.`);
+  } else {
+    console.log(`Image ${fileName} does not contain any faces.`);
+  }
+
+  // Delete the temporary file
+  await fs.unlinkSync(tempFilePath);
+});
+
+// Export the bucket name for use in other components
+exports.bucketName = dstBucketName;
--- src/PersonalPhotos.jsx
+++ src/PersonalPhotos.jsx
@@ -6,6 +6,7 @@ import {
   getMetadata,
   deleteObject,
   getDownloadURL,
+  uploadBytesResumable
 } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
@@ -41,6 +42,27 @@ const PersonalPhotos = () => {
     setPhotos(photos.filter((photo) => photo.ref !== photoRef));
   };
 
+  const handleUpload = (event) => {
+    event.preventDefault();
+    const selectedFile = event.target.files[0];
+    const storageRef = ref(storage, `images/${selectedFile.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, selectedFile);
+
+    // Listen for state changes, errors, and completion of the upload.
+    uploadTask.on('state_changed', (snapshot) => {
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+    }, (error) => {
+      console.log('Upload error:', error);
+    }, async () => {
+      // Upload completed successfully, now get the download URL
+      const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+      console.log('File uploaded:', downloadURL);
+      setPhotos([...photos, { url: downloadURL, ref: uploadTask.snapshot.ref }]);
+    });
+  };
+
   return (
     <div>
       <h1>Your Personal Photos</h1>
@@ -52,8 +74,74 @@ const PersonalPhotos = () => {
           </div>
         ))}
       </div>
+      <form onSubmit={handleUpload}>
+        <input type="file" onChange={handleUpload} />
+        <button type="submit">Upload</button>
+      </form>
     </div>
   );
 };
 
 export default PersonalPhotos;
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import {
+//   getStorage,
+//   ref,
+//   listAll,
+//   getMetadata,
+//   deleteObject,
+//   getDownloadURL,
+// } from 'firebase/storage';
+// import { getAuth } from 'firebase/auth';
+
+// const PersonalPhotos = () => {
+//   const [photos, setPhotos] = useState([]);
+//   const storage = getStorage();
+//   const auth = getAuth();
+
+//   useEffect(() => {
+//     const fetchPhotos = async () => {
+//       const storageRef = ref(storage, 'images');
+//       const listResult = await listAll(storageRef);
+//       const currentUserUid = auth.currentUser.uid;
+
+//       const personalPhotos = [];
+
+//       for (const itemRef of listResult.items) {
+//         const metadata = await getMetadata(itemRef);
+//         if (metadata.customMetadata && metadata.customMetadata.uploadedBy === currentUserUid) {
+//           const photoUrl = await getDownloadURL(itemRef);
+//           personalPhotos.push({ url: photoUrl, ref: itemRef });
+//         }
+//       }
+
+//       setPhotos(personalPhotos);
+//     };
+
+//     fetchPhotos();
+//   }, [storage, auth]);
+
+//   const handleDelete = async (photoRef) => {
+//     await deleteObject(photoRef);
+//     setPhotos(photos.filter((photo) => photo.ref !== photoRef));
+//   };
+
+//   return (
+//     <div>
+//       <h1>Your Personal Photos</h1>
+//       <div>
+//         {photos.map((photo) => (
+//           <div key={photo.url}>
+//             <img src={photo.url} alt="User uploaded" width="200" />
+//             <button onClick={() => handleDelete(photo.ref)}>Delete</button>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default PersonalPhotos;
