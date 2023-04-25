d2f76af24295ace752950eb842f74d5ad7fe951f
Natasha Bartley <+>
2023-04-25 16:11:03 -0400

    app paste

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import './App.css';
 import React from 'react';
 import NavBar from './NavBar';
-
-
+import Home from './Home';
 function App() {
-  return (
-    <div className="App">
-        <NavBar />
-    </div>
-  );
+ return (
+  <div className="App">
+    <NavBar />
+    <Home />
+  </div>
+ );
 }
-
 export default App;
