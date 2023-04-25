0d3920bd2dad3e2531f3bc1ffd857df3240dfc7f
Natasha Bartley <+>
2023-04-25 16:41:57 -0400

    move navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
 import './App.css';
-import React from 'react';
-import NavBar from './NavBar';
-import Home from './Home';
+import React from "react";
+import { Link, Route, Routes } from 'react-router-dom';
+import Home from "./Home";
+import Community from "./Community";
+import GigsPage from "./GigsPage";
+import About from "./About";
+import Create from "./Create";
+
 function App() {
  return (
   <div className="App">
-    <NavBar />
-    <Home />
+    <div>
+          <nav>
+            <ul className="navBar">
+                <li> 
+                    <Link to='/'>home</Link> 
+                </li>
+                <li> 
+                    <Link to='/Community'>community</Link>
+                </li>
+                <li>
+                    <Link to='/GigsPage'>gigs</Link>
+                </li>
+                <li> 
+                    <Link to='/About'>about</Link>
+                </li>
+                <li> 
+                    <Link to='/Create'>create</Link>
+                </li>
+            </ul>
+                
+          </nav>
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/Community" element={<Community />}/>
+                <Route path="/GigsPage" element={<GigsPage />}/>
+                <Route path="/About" element={<About />}/>
+                <Route path="/Create" element={<Create />}/>
+            </Routes>
+        </div>
+
   </div>
  );
 }
