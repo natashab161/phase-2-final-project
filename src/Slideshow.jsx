25605e3d9778468f875405df58e64249489fa5e3
Natasha Bartley <+>
2023-04-26 17:49:45 -0400

    commit before leaving school today

--- src/Slideshow.jsx
+++ src/Slideshow.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState,useEffect} from "react";
 
 const imageSlides = ["#0088FE", "#00C49F", "#FFBB28"];
+const delay  = 5000;
 
 function Slideshow() {
+    const [index, setIndext] = useState(0);
+
+    useEffect(() =>{
+        setTimeout(
+            () =>
+                setIndext((prevIndex) =>
+                    prevIndex === imageSlides.length - 1 ? 0 : prevIndex +  1 
+                ),
+             delay
+         );
+    })
+
     return (
         <div className="slideshow">
-            <div className="slideshowSlider">
+            <div 
+                className="slideshowSlider"
+                style={{ transform: `translate3d(${-index * 100}%)`}}
+                >
                 <div className="slide">
                     {imageSlides.map((backgroundImage, index) => (
                         <div className="slide" key={index} style={{backgroundImage}} /> 
+                    ))}
+                </div>
+                <div className="slideshowDots">
+                    {imageSlides.map((_, idx) => (
+                        <div key={idx} className="slideshowDot"></div>
                     ))}
                 </div>
             </div>
