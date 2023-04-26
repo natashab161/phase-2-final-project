383786a79d4a2f50f9c977ddf39862c0047c4984
Natasha Bartley <+>
2023-04-26 14:50:34 -0400

    search looking okay, not in final form

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import EventsPage from "./EventsPage";
 import SearchEvents from "./SearchEvents";
 import FeaturedEvents from "./FeaturedEvents";
+import CarouselHeader from "./CarouselHeader";
 
 function Home(){
     return(
         <div>
             <h1>the place to be for local artists and art-lovers to connect and grow</h1>
+            <CarouselHeader />
             <FeaturedEvents />
             <SearchEvents />
             <EventsPage />
