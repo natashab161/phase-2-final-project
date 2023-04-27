a3519c67fc32e2747461c4eabcac2138cba6b3e4
Jolie Gielchinsky <jolieschae@gmail.com>
2023-04-27 11:12:12 -0400

    event cards & container

--- src/EventsPage.jsx
+++ src/EventsPage.jsx
@@ -1,26 +1,46 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import EventCard from "./EventCard";
+import "./eventCard.css";
 
 function EventsPage() {
-  
-  const [events, setEvents] = useState([])
+  const [events, setEvents] = useState([]);
+  const [visibleEvents, setVisibleEvents] = useState([]);
+  const [startIndex, setStartIndex] = useState(0);
 
   useEffect(() => {
-  fetch("http://localhost:3000/events")
-  .then(response => response.json())
-  .then(data => {setEvents(data)})
-  }, [])
+    fetch("http://localhost:3000/events")
+      .then((response) => response.json())
+      .then((data) => {
+        setEvents(data);
+        setVisibleEvents(data.slice(0, 6)); // set initial visible events to the first 6
+      });
+  }, []);
 
-  const renderEventCards = events.map((event) => {
-    return (
-     <EventCard key={event.id} event={event}></EventCard>
-    )
-   })
-   return (
-    <div className="event-container">
-     {renderEventCards}
-    </div>
-   );
+  const handlePrevClick = () => {
+    setStartIndex(Math.max(0, startIndex - 6)); // update the start index to show previous 6 events
+    setVisibleEvents(events.slice(startIndex - 6, startIndex)); // update the visible events
+  };
+
+  const handleNextClick = () => {
+    setStartIndex(startIndex + 6); // update the start index to show next 6 events
+    setVisibleEvents(events.slice(startIndex + 6, startIndex + 12)); // update the visible events
+  };
+
+  const renderEventCards = visibleEvents.map((event) => {
+    return <EventCard key={event.id} event={event}></EventCard>;
+  });
+
+  return (
+    <div>
+      <div className="eventsContainer">
+      {renderEventCards}
+      </div>
+    <div class="button-container">
+    {startIndex > 0 && <button class="pageButton"onClick={handlePrevClick}>Prev</button>}
+    {startIndex + 6 < events.length && (
+      <button class="pageButton" onClick={handleNextClick}>Next</button>)}</div>
+      </div>
+  );
 }
 
 export default EventsPage;
